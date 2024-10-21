c315e523b9d14ad28420d8bfe70ef6bc98f542ee
Zac <zac@simplepixels.com.au>
2024-10-21 11:39:46 +1100

    First Commit

---
+++ Assets/Scripts/OuterGame/ShipController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float turnSpeed = 90.0f;
+    public float horizontalInput;
+    public float forwardInput;
+    public bool canMove = false; // New flag to control movement lock
+    private ShopLogic ShopLogic;
+    private float startingY;
+    public int dayLimit = 3; // Initial day limit
+
+    void Start()
+    {
+        startingY = transform.position.y;
+        ShopLogic = FindObjectOfType<ShopLogic>(); // Assuming ShopLogic exists in the scene
+    }
+
+    void Update()
+    {
+        if (!canMove)
+        {
+            // Don't allow movement if canMove is false
+            return;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        float speed = 10.0f;
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput, Space.Self); // Move the ship forward based on the vertical input
+
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput); // Rotate the ship based on the horizontal input
+        float steeringFactor = Mathf.Clamp01(Mathf.Abs(forwardInput));
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput * steeringFactor); 
+
+        KeepShipWithinBounds();
+        MaintainStartingY();
+    }
+
+    void KeepShipWithinBounds() // Keep the ship within the bounds of the game area
+    {
+        if (transform.position.x < -50)
+        {
+            transform.position = new Vector3(-50, startingY, transform.position.z);
+        }
+        else if (transform.position.x > 50)
+        {
+            transform.position = new Vector3(50, startingY, transform.position.z);
+        }
+        else if (transform.position.z < -50)
+        {
+            transform.position = new Vector3(transform.position.x, startingY, -50);
+        }
+        else if (transform.position.z > 50)
+        {
+            transform.position = new Vector3(transform.position.x, startingY, 50);
+        }
+    }
+
+    void MaintainStartingY()  // Maintain the starting Y position of the ship
+    {
+        transform.position = new Vector3(transform.position.x, startingY, transform.position.z);
+    }
+
+    // Day limit management
+    public void DecreaseDayLimit()
+    {
+        if (dayLimit > 0)
+        {
+            dayLimit--;
+            Debug.Log($"Day limit decreased. Days remaining: {dayLimit}");
+        }
+        else
+        {
+            Debug.Log("No more days left to interact with islands.");
+        }
+    }
+
+    public void ResetDayLimit()
+    {
+        dayLimit = 3;
+        Debug.Log("Day limit reset to 3.");
+    }
+
+    public bool CanInteractWithIsland()
+    {
+        return dayLimit > 0;
+    }
+
+    // Enable or disable player movement
+    public void SetMovement(bool enable)
+    {
+        canMove = enable;
+    }
+}
